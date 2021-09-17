2174d41af62eee7647400bf86600bb468a1c3479
Dron2019 <mr.andrey@bigmir.net>
2021-09-17 13:56:02 +0300

    form model and form view add

---
+++ src/assets/s3d/scripts/modules/callbackForm/callbackFormModel.js
@@ -0,0 +1,8 @@
+import EventEmitter from '../eventEmitter/EventEmitter';
+
+export default class CallbackFormModel extends EventEmitter {
+  constructor(data) {
+    super();
+    // this.view = new data;
+  }
+}
